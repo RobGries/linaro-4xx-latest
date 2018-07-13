52ae429ce3c8dd54b3ec513963923771e29b013b
Rob Gries <bob2600@gmail.com>
2018-07-13 14:58:25 -0400

    set pclk for ov5640 as per mani's suggestion. otherwise it all fails.

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -2006,8 +2006,13 @@ static int ov5640_init_controls(struct ov5640_dev *sensor)
 	/* Auto/manual gain */
 	ctrls->auto_gain = v4l2_ctrl_new_std(hdl, ops, V4L2_CID_AUTOGAIN,
 					     0, 1, 1, 1);
+
 	ctrls->gain = v4l2_ctrl_new_std(hdl, ops, V4L2_CID_GAIN,
 					0, 1023, 1, 0);
+
+	/* Pixel clock (default of 96MHz) */
+	ctrls->pixel_clock = v4l2_ctrl_new_std(hdl, ops, V4L2_CID_PIXEL_RATE,
+					1, INT_MAX, 1, 96000000);
 
 	ctrls->saturation = v4l2_ctrl_new_std(hdl, ops, V4L2_CID_SATURATION,
 					      0, 255, 1, 64);
